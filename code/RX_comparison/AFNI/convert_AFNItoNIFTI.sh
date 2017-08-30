b41eff1a42bf0fcd242cba4eb6d96d2467d67fdb
Dani Cosme <dani.cosme@gmail.com>
2017-08-30 11:40:40 -0700

    added script to convert AFNI to NIFTI files

---
+++ code/RX_comparison/AFNI/convert_AFNItoNIFTI.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+. ~/.bashrc
+
+# Define path to AFNI results directory, model names, and contrast numbers
+resultsDir=/Volumes/psych-cog/dsnlab/MDC/functional-workshop/results/AFNI
+models=(3Ts all)
+selfOther=13
+age_selfOther=23
+age2_selfOther=25
+
+# Convert AFNI contrasts to nifti files
+# AFNI documentation https://afni.nimh.nih.gov/pub/dist/doc/program_help/3dAFNItoNIFTI.html
+for model in "${models[@]}"; do
+	3dAFNItoNIFTI -prefix "${resultsDir}"/self-other_"${model}" "${resultsDir}"/"${model}"+tlrc["${selfOther}"]
+	3dAFNItoNIFTI -prefix "${resultsDir}"/age.self-other_"${model}" "${resultsDir}"/"${model}"+tlrc["${age_selfOther}"]
+	3dAFNItoNIFTI -prefix "${resultsDir}"/age2.self-other_"${model}" "${resultsDir}"/"${model}"+tlrc["${age2_selfOther}"]
+done
